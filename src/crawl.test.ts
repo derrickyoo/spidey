7400c76e79128fcfe81ec9f6ca69591b530f46e5
Derrick Yoo <derrickyoo@gmail.com>
2026-03-12 10:32:08 -0700

    add tests

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,4 +1,4 @@
-import { normalizeURL } from "./crawl.js";
+import { getHeadingFromHTML, normalizeURL } from "./crawl";
 
 test("normalizeURL http protocol", () => {
   const input = "http://www.boot.dev/blog/path/";
@@ -25,5 +25,33 @@ test("normalizeURL domain name", () => {
   const input = "http://CRAWLER-TEST.com/path";
   const actual = normalizeURL(input);
   const expected = "crawler-test.com/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML basic", () => {
+  const inputBody = `<html><body><h1>Test Title</h1></body></html>`;
+  const actual = getHeadingFromHTML(inputBody);
+  const expected = "Test Title";
+  expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML basic", () => {
+  const inputBody = `<html><body><h1>Test Title</h1></body></html>`;
+  const actual = getHeadingFromHTML(inputBody);
+  const expected = "Test Title";
+  expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML missing h1", () => {
+  const inputBody = `<html><body></body></html>`;
+  const actual = getHeadingFromHTML(inputBody);
+  const expected = "";
+  expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML fallback h2", () => {
+  const inputBody = `<html><body><h2>Fallback</h2></body></html>`;
+  const actual = getHeadingFromHTML(inputBody);
+  const expected = "Fallback";
   expect(actual).toEqual(expected);
 });
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,3 +1,5 @@
+import { JSDOM } from "jsdom";
+
 export function normalizeURL(url: string) {
   const urlObj = new URL(url);
   let fullPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -7,4 +9,12 @@ export function normalizeURL(url: string) {
   }
 
   return fullPath;
+}
+
+export function getHeadingFromHTML(html: string): string {
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
+  const heading = document.querySelector("h1") ?? document.querySelector("h2");
+
+  return (heading?.textContent ?? "").trim();
 }
